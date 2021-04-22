4e9b65870ee6cdb232776ac906dae654458da6b0
Ashwani <imakumar98@gmail.com>
2021-04-22 16:19:18 +0530

    text and input uis

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Button title="Click here"></Button>
+      <Text style={styles.heading}>Login Form</Text>
+      
+      <TextInput placeholder="Type your username"></TextInput>
+
+      <TextInput placeholder="Type your password"></TextInput>
+
+      <Button title="Login"></Button>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +21,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e5e5e5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  heading: {
+    fontWeight: 'bold',
+  }
 });
